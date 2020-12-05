be65aa042288f679a57e26a748f144becd917a1f
lypxynok <lypxynok@163.com>
2020-12-05 11:48:07 +0800

    [修改] 去掉无效引用

--- java-code-demo/src/main/java/org/xynok/MainApp.java
+++ java-code-demo/src/main/java/org/xynok/MainApp.java
@@ -4,8 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Import;
-import org.xynok.tools.datasource.CustomDataSourceBeanRegister;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 // @Import(CustomDataSourceBeanRegister.class)
